32d987628b8ec50b328fe0619c3aeb4a4bdd8c74
Joy Das <joydascsepuc@gmail.com>
2022-02-16 19:23:36 +0600

    configuring the passport in the application

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,11 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
