92e875e923c72dcc7b95424ea94382562ec6934e
Joy Das <joydascsepuc@gmail.com>
2022-02-16 10:55:31 +0600

    Adding migration for products & reviews table with foreign key constrained

--- database/migrations/2022_02_16_035955_create_products_table.php
+++ database/migrations/2022_02_16_035955_create_products_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('details');
+            $table->double('price');
+            $table->integer('stock');
+            $table->double('discount');
             $table->timestamps();
         });
     }
